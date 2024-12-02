9d5c295c94e5f99a5a0a8b251555de9462caf003
matteocaschetto <matteocaschetto9@gmail.com>
2024-12-02 16:35:25 +0100

    u3-w10-d1-hw

---
+++ src/components/CommentArea.jsx
@@ -0,0 +1,51 @@
+import { Component } from "react";
+import CommentList from "./CommentList";
+import AddComment from "./AddComment";
+import Loading from "./Loading";
+import Error from "./Error";
+
+class CommentArea extends Component {
+  state = {
+    comments: [],
+    isLoading: true,
+    isError: false
+  };
+
+  componentDidMount = async () => {
+    try {
+      let response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/comments/" +
+          this.props.asin,
+        {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzM3MTgxZjhhZDEyOTAwMTU4NzZiZTQiLCJpYXQiOjE3MzMxNTM0OTYsImV4cCI6MTczNDM2MzA5Nn0.7WnWGgIP5Na7KG58ofiwAKyjUMOE-DTRKJt4yYDX8Hc"
+          }
+        }
+      );
+      console.log(response);
+      if (response.ok) {
+        let comments = await response.json();
+        this.setState({ comments: comments, isLoading: false, isError: false });
+      } else {
+        this.setState({ isLoading: false, isError: true });
+      }
+    } catch (error) {
+      console.log(error);
+      this.setState({ isLoading: false, isError: true });
+    }
+  };
+
+  render() {
+    return (
+      <div className="text-center">
+        {this.state.isLoading && <Loading />}
+        {this.state.isError && <Error />}
+        <AddComment asin={this.props.asin} />
+        <CommentList commentsToShow={this.state.comments} />
+      </div>
+    );
+  }
+}
+
+export default CommentArea;
